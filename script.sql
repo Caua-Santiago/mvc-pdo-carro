53836d0c4e2b7384e3f01533c5dc6598cb3b93ba
Caua <cauasantiagofreitas23241@gmail.com>
2026-09-02 22:00:03 -0300

    First Commit

---
+++ script.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE	ESTACIONAMENTO;
+USE estacionamento;
+
+CREATE TABLE carro (
+	id_carro INT AUTO_INCREMENT PRIMARY KEY,
+    modelo   VARCHAR(50) NOT NULL,
+    placa	 VARCHAR(20) NOT NULL,
+);
+
+INSERT INTO carro (modelo,placa) VALUES ('Onix', 'ABC1D23');
+INSERT INTO carro (modelo,placa) VALUES ('Corolla', 'UVW1X23');
+INSERT INTO carro (modelo,placa) VALUES ('Renegade', 'BCD7E89');
+
+SELECT * FROM carro;
+SELECT * FROM carro WHERE id = 2;
+
+UPDATE carro SET modelo = 'HR-V', placa = 'MNO5P67' WHERE id = 3;
+
+DELETE FROM carro WHERE id = '1'
+
+
+
